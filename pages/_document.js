10d8b57d01d1c9317ba8110f1a0ad1862485fc8a
XiaYue <44867741+XiaYue1013@users.noreply.github.com>
2024-03-17 21:45:55 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4677299438632389" crossorigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
