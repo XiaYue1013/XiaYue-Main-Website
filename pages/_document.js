c2ee564db91238783abe286e2f9cbc7399f3e9c8
XiaYue <44867741+XiaYue1013@users.noreply.github.com>
2024-03-17 21:35:30 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-4677299438632389">
+      </Head>
       <body>
         <Main />
         <NextScript />
